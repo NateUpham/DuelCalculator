427146600948f6d82047645ccc19c303cafd4d88
NateUpham <67931576+NateUpham@users.noreply.github.com>
2020-09-03 09:58:13 -0500

    Update duelCalc.js

--- duelCalc.js
+++ duelCalc.js
@@ -25,7 +25,7 @@ p1Plus.addEventListener("click", function(){
 p1Sub.addEventListener("click", function(){
 	if(!gameOver){
 	p1Score = p1Score - (Number(document.getElementById("player1").value));  
-	if(p1Score === 0){
+	if(p1Score <= 0){
 		document.querySelector("#p1Name").textContent = "Player 2 Wins"
 		document.querySelector("#p2Name").textContent = "Player 2 Wins"
 		gameOver = true;
@@ -47,7 +47,7 @@ p2Plus.addEventListener("click", function(){
 p2Sub.addEventListener("click", function(){
 	if(!gameOver){
 	p2Score = p2Score - (Number(document.getElementById("player2").value)); 
-	 if(p2Score === 0){
+	 if(p2Score <= 0){
 		document.querySelector("#p2Name").textContent = "Player 1 Wins"
 		document.querySelector("#p1Name").textContent = "Player 1 Wins"
 		gameOver = true;
